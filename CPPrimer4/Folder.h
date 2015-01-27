28e4f41f8c0d347f3aa41e358290c65ed5296250
lennon622@gmail.com <TL.2215738819.tl>
2015-01-27 23:32:38 +0800

    完善

--- CPPrimer4/Folder.h
+++ CPPrimer4/Folder.h
@@ -2,8 +2,22 @@
 #define Folder_h__
 
 #include "Message.h"
+#include <set>
+
+class Message;
 
 class Folder {
+private:
+	std::set<Message*> messages;
+
+public:
+	void add(Message* ptrM) {
+		messages.insert(ptrM);
+	}
+
+	void remove(Message* ptrM) {
+		messages.erase(ptrM);
+	}
 
 };
 
--- CPPrimer4/Message.h
+++ CPPrimer4/Message.h
@@ -3,15 +3,26 @@
 
 #include <string>
 #include "Folder.h"
+#include <set>
 
 class Message {
 private:
 	std::string contents;
+	std::set<Folder *> folders;
+	void putMessageInFolders(const std::set<Folder*>&);
+	void removeMessageFromFolder();
 
+	void addFolder(Folder&);
+	void removeFolder(Folder&);
 
 public:
-	Message* save(Folder& f);
+	Message(const std::string& str = "") : contents(str) {}
+	Message(const Message&);
+	Message& operator=(const Message&);
+	~Message();
 
+	void save(Folder&);
+	void remove(Folder&);
 };
 
 
